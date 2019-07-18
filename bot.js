fae6d848956d8efdb889ba8fe04c2a1114853f09
bodyking <39631256+bodyking@users.noreply.github.com>
2019-07-18 17:30:22 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("A.AMR10");
 var prefix = "7";
 client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "la")) {
+            if (message.content.startsWith(prefix + "7")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
@@ -26,5 +26,22 @@ client.on('ready', () => {
 client.user.setGame(`7bc`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
+
+client.on('message', message => {  
+    if (message.author.bot) return; ///Pixel Team
+    if (message.content.startsWith(prefix + 'clear')) { 
+    if(!message.channel.guild) return message.reply(`** This Command For Servers Only**`); 
+     if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(`** You don't have Premissions!**`);
+     if(!message.guild.member(client.user).hasPermission('MANAGE_GUILD')) return message.channel.send(`**I don't have Permission!**`);
+    let args = message.content.split(" ").slice(1)
+    let messagecount = parseInt(args);
+    if (args > 100) return message.reply(`** The number can't be more than **100** .**`).then(messages => messages.delete(5000))
+    if(!messagecount) args = '100';
+    message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages)).then(msgs => {
+    message.channel.send(`** Done , Deleted \`${msgs.size}\` messages.**`).then(messages => messages.delete(5000));
+    })
+  }
+});  ///Zine & Zaid
+
 
 client.login(process.env.BOT_TOKEN);
