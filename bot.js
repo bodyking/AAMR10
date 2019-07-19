7dccb4f489fc228bd951843f58a943749d2a8080
bodyking <39631256+bodyking@users.noreply.github.com>
2019-07-19 14:14:20 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,6 +146,8 @@ message.author.sendMessage(`
 
 ^kick
 
+^ping
+
 -------------------------------------------------------
 
 رابط البوت:https://discordapp.com/oauth2/authorize?client_id=445026531341762560&permissions=8&scope=bot
@@ -165,6 +167,28 @@ var ApL = `${Math.round(client.ping)}`
  })
   }  
  });
+
+client.on('guildMemberAdd', member => {
+
+    const channel = member.guild.channels.find('name', 'welcome');
+  
+    const millis = new Date().getTime() - member.user.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+
+
+
+
+  
+    const embed = new Discord.RichEmbed()
+    
+    .setColor("black")
+    .setDescription(`**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**`)
+    .setAuthor(member.user.tag, member.user.avatarURL);
+    channel.sendEmbed(embed);
+
+  
+});
 
 
 client.login(process.env.BOT_TOKEN);
