ed15fb9d1f181f2efbce1088f83042e5a8c48f4f
bodyking <39631256+bodyking@users.noreply.github.com>
2019-07-20 01:17:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -201,4 +201,54 @@ client.on('message', message => {
 });
 
 
+var id = ['409045670943784974'] //تذكر إذا كنت تبي تضيف شخص معاك حط فاصله وبعدين العلامه '
+var initcmd = '^'
+const developers = 409045670943784974
+const adminprefix = initcmd;
+client.on('message', message => {
+    var argresult = message.content.split(' ').slice(2).join(" ")
+      if (!developers.includes(message.author.id)) return;
+     
+  if (message.content.startsWith(adminprefix + 'vip ply')) {
+    if (!argresult) return message.channel.send(':no_entry: | يرجى كتابة أي شيء بعد ألأمر')
+    client.user.setGame(argresult);
+      message.channel.send("**:white_check_mark: | The Playing Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else
+  if (message.content.startsWith(adminprefix + 'vip wat')) {
+        if (!argresult) return message.channel.send(':no_entry: | يرجى كتابة أي شيء بعد ألأمر')
+ 
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send("**:white_check_mark: | The Watching Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else
+  if (message.content.startsWith(adminprefix + 'vip lis')) {
+        if (!argresult) return message.channel.send(':no_entry: | يرجى كتابة أي شيء بعد ألأمر')
+ 
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send("**:white_check_mark: | The Listening Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else
+  if (message.content.startsWith(adminprefix + 'vip stream')) {
+        if (!argresult) return message.channel.send(':no_entry: | يرجى كتابة أي شيء بعد ألأمر')
+ 
+    client.user.setGame(argresult, "https://www.twitch.tv/fadi_games2");
+      message.channel.send("**:white_check_mark: | The Streaming Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  }
+  if (message.content.startsWith(adminprefix + 'vip username')) {
+        if (!argresult) return message.channel.send(':no_entry: | يرجى كتابة أي شيء بعد ألأمر')
+ 
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'vip avatar')) {
+      if (!argresult) return message.channel.send(':no_entry: | يرجى كتابة أي شيء بعد ألأمر')
+ 
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+
 client.login(process.env.BOT_TOKEN);
