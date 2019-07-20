b5758ad2ca36f03a39ed86055ce8014445ebeb23
bodyking <39631256+bodyking@users.noreply.github.com>
2019-07-20 03:44:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -977,7 +977,7 @@ client.on("message", message => {
 	if( !message.guild ) return;
 	if( !msg.startsWith( prefix + 'role' ) ) return;
 	if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(' **__ليس لديك صلاحيات__**');
-	if( msg.toLowerCase().startsWith( prefix + '-role' ) ){
+	if( msg.toLowerCase().startsWith( prefix + 'remove' ) ){
 		if( !args[0] ) return message.reply( '**:x: يرجى وضع الشخص المراد سحب منه الرتبة**' );
 		if( !args[1] ) return message.reply( '**:x: يرجى وضع الرتبة المراد سحبها من الشخص**' );
 		var role = msg.split(' ').slice(2).join(" ").toLowerCase(); 
